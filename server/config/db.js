d18e3785a5797b348d1802efe1df4ebb4ffe76a0
Yaaseen Khan <yaaseenkhan02@gmail.com>
2021-10-30 16:47:16 -0400

    updated to make everything work with mongodb

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,5 @@
 module.exports = 
 {
-	"URI": "mongodb://localhost/book_store"
+	// "URI": "mongodb://localhost/book_store"
+	"URI":  "mongodb+srv://Yaaseen:yLA81bGj1KI2Yy32@mongodbserver.ty9ef.mongodb.net/book_store?retryWrites=true&w=majority"
 }
